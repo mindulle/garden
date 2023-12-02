04207e1ebd19458ce5c24852b2dfcd279a97a30b
mindulle <mindullestudio@gmail.com>
2023-12-02 11:39:15 +0000

    vault vackup

---
+++ quartz.config.ts
@@ -0,0 +1,77 @@
+import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
+
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "Mindulle Garden",
+    enableSPA: true,
+    enablePopovers: true,
+    analytics: {
+      provider: "plausible",
+    },
+    baseUrl: "mindulle.github.io/garden",
+    ignorePatterns: ["private", "templates", "_templates", ".obsidian"],
+    defaultDateType: "created",
+    theme: {
+      typography: {
+        header: "Schibsted Grotesk",
+        body: "Source Sans Pro",
+        code: "IBM Plex Mono",
+      },
+      colors: {
+        lightMode: {
+          light: "#FDFDF5",
+          lightgray: "#E0E4D6",
+          gray: "#74796D",
+          darkgray: "#44483E",
+          dark: "#1A1C18",
+          secondary: "#56624B",
+          tertiary: "#755B00",
+          highlight: "rgba(188, 242, 147, 0.15)",
+        },
+        darkMode: {
+          light: "#1A1C18",
+          lightgray: "#44483E",
+          gray: "#8E9286",
+          darkgray: "#C4C8BB",
+          dark: "#C7C7C0",
+          secondary: "#BECBAE",
+          tertiary: "#F0C131",
+          highlight: "rgba(88, 68, 0, 0.15)",
+        },
+      },
+    },
+  },
+  plugins: {
+    transformers: [
+      Plugin.FrontMatter(),
+      Plugin.TableOfContents(),
+      Plugin.CreatedModifiedDate({
+        priority: ["frontmatter", "filesystem"], // you can add 'git' here for last modified from Git but this makes the build slower
+      }),
+      Plugin.SyntaxHighlighting(),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.GitHubFlavoredMarkdown(),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.Description(),
+    ],
+    filters: [Plugin.RemoveDrafts()],
+    emitters: [
+      Plugin.AliasRedirects(),
+      Plugin.ComponentResources({ fontOrigin: "googleFonts" }),
+      Plugin.ContentPage(),
+      Plugin.FolderPage(),
+      Plugin.TagPage(),
+      Plugin.ContentIndex({
+        enableSiteMap: true,
+        enableRSS: true,
+      }),
+      Plugin.Assets(),
+      Plugin.Static(),
+      Plugin.NotFoundPage(),
+    ],
+  },
+}
+
+export default config
